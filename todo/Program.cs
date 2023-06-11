7820287e5b312220953c513aa2d5ab7eedf52702
Amir JCS <amirmahdyjdx@gmail.com>
2023-06-12 02:05:39 +0330

    Add the no-arguments section

---
+++ todo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace todo
+{
+    public class todo
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length == 0){
+                System.Console.WriteLine("Welcome to the todo cli \n\n");
+                
+                Console.Write("Use this tempelate : \n\t");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("$ ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("todo");
+                Console.ResetColor();
+                Console.Write(" [Command] [Command Options] \n\n\n");
+
+                Console.WriteLine("Use --help for learn commands \n");
+            }
+        }
+    }
+}
