70215a817e2b32502d8fd024f5cfa9e6aac238d4
Amir JCS <amirmahdyjdx@gmail.com>
2023-06-12 03:21:31 +0330

    Add --help flag

--- todo/Program.cs
+++ todo/Program.cs
@@ -6,9 +6,10 @@ namespace todo
     {
         public static void Main(string[] args)
         {
-            if(args.Length == 0){
+            if (args.Length == 0)
+            {
                 System.Console.WriteLine("Welcome to the todo cli \n\n");
-                
+
                 Console.Write("Use this tempelate : \n\t");
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.Write("$ ");
@@ -18,6 +19,35 @@ namespace todo
                 Console.Write(" [Command] [Command Options] \n\n\n");
 
                 Console.WriteLine("Use --help for learn commands \n");
+            }
+            else
+            {
+                // --help flag
+                if (args[0] == "--help")
+                {
+                    Console.WriteLine("Todo CLI commands : \n\n");
+
+                    Console.WriteLine("  Command │ Options ");
+                    Console.WriteLine(" ─────────┼─────────");
+                    static void _command(string title, string option, string tip = "")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write($"  {title}");
+                        Console.ResetColor();
+                        Console.Write($" {"".PadLeft(7 - title.Length,' ')}│ ");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write($"{option}");
+                        Console.ResetColor();
+                        Console.WriteLine($"   {tip}");
+                    }
+
+                    _command("add","[Task Title]");
+                    _command("add-sub","[Task ID] [sub Task title]");
+                    _command("fin","[Task Id] - [Sub task id]","input [Todo id] if dose not sub work exists...");
+                    _command("see","[EMPTY]");
+
+                    System.Console.WriteLine("\n");
+                }
             }
         }
     }
