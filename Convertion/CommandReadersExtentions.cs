c5083ac399057fac168b67b2237375f82ef51e6b
Amir JCS <amirmahdyjdx@gmail.com>
2023-06-19 18:04:45 +0330

    feat: add commands convertor method

--- Convertion/CommandReadersExtentions.cs
+++ Convertion/CommandReadersExtentions.cs
@@ -3,6 +3,21 @@
     public static class CommandReadersExtentions
     {
         public static IEnumerable<string> Flags(this IEnumerable<string> termArgs) => termArgs.Where(x => x != null && x.StartsWith("--")).ToList();
+
+        public static IEnumerable<string> Commands(this IEnumerable<string> termArgs)
+        {
+            IEnumerable<string> res = new List<string>();
+            for (int i = 0; i < termArgs.Count(); i++)
+            {
+                if (!termArgs.ToList()[i].StartsWith("--"))
+                {
+                    return res;
+                }
+
+                res.ToList().Add(termArgs.ToList()[i]);
+            }
+            return res;
+        }
        
     }
 }
